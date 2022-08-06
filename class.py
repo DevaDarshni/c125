011bb114494a23dbce53e94806eca5cf910bce17
DevaDarshni <85098562+DevaDarshni@users.noreply.github.com>
2022-08-06 12:13:15 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,19 @@
+class Car(object):
+
+    def __init__(self,model,color,company,speed_limit):
+        self.color=color
+        self.company=company
+        self.speed_limit=speed_limit
+        self.model=model
+
+def start(self):
+    print("started")
+
+def start(self):
+    print("stopped")
+
+def accelarate(self):
+    print("accelarating")
+
+audi=Car("ai","blue","audi","200")
+audi.color
